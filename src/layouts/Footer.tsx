85c6fd8070de6a1b5443934b9ed3b59901a215a1
Ekaterina <katya.leemi@gmail.com>
2023-04-24 12:39:05 +0300

    Added media queries to footer, fixed bug with form slogan

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -10,13 +10,16 @@ const Footer = () => {
 
     const randomFact = facts[facts.length * Math.random() | 0]
     return (
-        <footer>
+        <footer className={styles.footer}>
             <div className={styles.left}>
                 <h2>Ekaterina<br /><span className={styles.textAccent}>Orlova</span></h2>
                 <p>Random fact about me:<br /> {randomFact}</p>
             </div>
             <div className={styles.right}>
-        
+                <ul className={styles.list}>
+                    <li><a href='mailto:katya.leemi@gmail.com'>katya.leemi@gmail.com</a></li>
+                    <li><a href='./Orlova_CV_Fullstack.pdf' target="_blank" rel="noopener noreferrer">CV</a></li>
+                </ul>
             </div>
 
         </footer>
